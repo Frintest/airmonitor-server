1ccb17aa26b86180d9391067f606a228e5b5242c
root <root@2011357-cg28939.twc1.net>
2024-05-11 21:45:25 +0300

    v1

---
+++ db.js
@@ -0,0 +1,14 @@
+const mysql = require("mysql2/promise");
+
+const setConnection = async () => {
+	const connection = await mysql.createPool({
+		host: 'localhost', // default
+		port: 3306, // default
+		user: 'sensor_user',
+		database: 'sensor',
+		password: 'jshdf^%hfjDfsbh$'
+	});
+	return connection;
+};
+
+module.exports = setConnection;
---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+const PORT = 3001;
+const app = express();
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+	cors: {
+		origin: ['http://localhost:3000', 'http://airmonitor.servermc.ru']
+	}
+});
+
+const setConnection = require('./db.js');
+
+const airStateSocket = require('./sockets/air-state.js');
+
+io.on('connection', async (socket) => {
+	console.log('Server connected');
+
+	const db_connection = await setConnection();
+
+	await airStateSocket(socket, db_connection);
+	let airStateInterval = setInterval(async () => {
+		await airStateSocket(socket, db_connection);
+	}, 3000);
+
+	socket.on("disconnect", () => {
+		console.log(`Socket ${socket.id} disconnect`);
+		clearInterval(airStateInterval);
+	});
+});
+
+httpServer.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+});
