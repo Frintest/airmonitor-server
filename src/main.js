6175dacb0aae8adf6dc04f4848fa26a9dba120f8
Dmitry <dimaruzaev2@gmai.com>
2024-08-12 13:39:54 +0300

    ref

--- src/main.js
+++ src/main.js
@@ -8,7 +8,6 @@ export const main = (io) => {
       console.log(`Socket ${socket.id} connect`);
 
       const db_connection = setDBConnection();
-
       airStateHandlers(socket, db_connection);
       airHistoryHandlers(socket, db_connection);
       airStandardsHandlers(socket, db_connection);
